9269e915e82c728a0a811742158faae694ba5856
TaniguchiYuya <2020m118@kuas.ac.jp>
2020-07-10 03:04:01 +0900

    Made a radio button

--- app/src/main/java/com/example/project09/timer.java
+++ app/src/main/java/com/example/project09/timer.java
@@ -2,16 +2,17 @@ package com.example.project09;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class Create_a_calendar extends AppCompatActivity {
+
+public class timer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_a_calendar);
-    }
+        setContentView(R.layout.activity_timer);
 
+
+
+    }
 }
