63c0a64d893e89aac6e38f0f5723ea823c1d7ef4
TaniguchiYuya <2020m118@kuas.ac.jp>
2020-07-10 01:05:55 +0900

    I made another activity

---
+++ app/src/main/java/com/example/project09/Create_a_calendar.java
@@ -0,0 +1,14 @@
+package com.example.project09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_a_calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_a_calendar);
+    }
+}
--- app/src/main/java/com/example/project09/MainActivity.java
+++ app/src/main/java/com/example/project09/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.project09;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View view) {
+        Intent Create_a_calendar = new Intent(this, Create_a_calendar.class);
+        startActivity(Create_a_calendar);
     }
 }
