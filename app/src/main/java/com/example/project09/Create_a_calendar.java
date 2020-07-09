63656aca798f03fb9ed77083f90d66540b6e07cb
TaniguchiYuya <2020m118@kuas.ac.jp>
2020-07-10 04:20:16 +0900

    time

--- app/src/main/java/com/example/project09/Create_a_calendar.java
+++ app/src/main/java/com/example/project09/Create_a_calendar.java
@@ -2,16 +2,76 @@ package com.example.project09;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class Create_a_calendar extends AppCompatActivity {
+
+   TextView mTimeTextView;
+
+    Button mPickTimeButton;
+
+    Context mContext = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_a_calendar);
-    }
 
+
+        mTimeTextView = (TextView) findViewById(R.id.time_text_view);
+
+        Calendar calendar = Calendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+
+        mPickTimeButton = (Button) findViewById(R.id.pick_time_button);
+        mPickTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        mTimeTextView.setText(hourOfDay + ":" + minute);
+                    }
+                }, hour, minute, android.text.format.DateFormat.is24HourFormat(mContext));
+                timePickerDialog.show();
+            }
+        });
+
+
+
+        mTimeTextView = (TextView) findViewById(R.id.time_text_view2);
+
+
+
+
+        mPickTimeButton = (Button) findViewById(R.id.pick_time_button2);
+        mPickTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        mTimeTextView.setText(hourOfDay + ":" + minute);
+                    }
+                },hour,minute,android.text.format.DateFormat.is24HourFormat(mContext));
+                timePickerDialog.show();
+            }
+        });
+    }
 }
+
+
