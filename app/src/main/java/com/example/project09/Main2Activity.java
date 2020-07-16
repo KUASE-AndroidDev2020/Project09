9980ce3027ade804370d0144b0e9dadd1961a837
TaniguchiYuya <2020m118@kuas.ac.jp>
2020-07-17 03:51:41 +0900

    calendar

--- app/src/main/java/com/example/project09/Main2Activity.java
+++ app/src/main/java/com/example/project09/Main2Activity.java
@@ -7,31 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
 
-public class Main2Activity extends AppCompatActivity {
+
+public class Main2Activity extends AppCompatActivity  implements OnDateChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ((CalendarView) findViewById(R.id.calendarView)).setOnDateChangeListener(listener);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(this);
     }
-    CalendarView.OnDateChangeListener listener = new CalendarView.OnDateChangeListener() {
 
-        /**
-         * 日付部分タップ時に実行される処理
-         * @param view 押下されたカレンダーのインスタンス
-         * @param year タップされた日付の「年」
-         * @param month タップされた日付の「月」※月は0月から始まるから、+1して調整が必要
-         * @param dayOfMonth タップされた日付の「日」
-         */
-        public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-            // とりあえずトースト表示してみる
-            Toast.makeText(getApplicationContext(), year + "年" + (month + 1) + "月" + dayOfMonth + "日がクリックされました", Toast.LENGTH_SHORT).show();
+    public void onSelectedDayChange(CalendarView view, int year, int month,
+                                    int dayOfMonth) {
 
-            // メモ画面へ遷移するIntent発行？
-            Intent calendar = new Intent(this,calendar.class);
-            startActivity(calendar);
-        }
+        Intent varIntent = new Intent(this, calendar.class);
+
+        startActivity(varIntent);
+
+    }
+
+
 }
